ac7860f5a70891c1b8b9b4e16ad195573bf75f05
Anna Morozova <anna.morozova@thoughtworks.com>
2020-03-25 10:39:50 -0300

    [Anna/Alvaro] fixing error translate movie to spanish

--- src/example/SamplePage/SamplePage.js
+++ src/example/SamplePage/SamplePage.js
@@ -3,7 +3,7 @@ import MediaList from "../MediaList/MediaList";
 
 const SamplePage = () =>
 	<div>
-		<MediaList title={"Movies"} api={"https://api.myjson.com/bins/1bqcdq"}/>
+		<MediaList title={"Peliculas"} api={"https://api.myjson.com/bins/1bqcdq"}/>
 		<MediaList title={"Series"} api={"https://api.myjson.com/bins/18q3ry"}/>
 	</div>
 

328474ee94bfcb7126cd01512ebacef00af3f148
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-04-20 18:35:32 -0400

    [Gonzalo] Add Prettier support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import {Router} from 'react-router-dom';
-import history from './services/history'
-import Routes from './components/Routes/Routes';
+import React from "react";
+import "./App.css";
+import { Router } from "react-router-dom";
+import history from "./services/history";
+import Routes from "./components/Routes/Routes";
 
 function App() {
   return (
     <Router history={history}>
-      <Routes/>
+      <Routes />
     </Router>
   );
 }

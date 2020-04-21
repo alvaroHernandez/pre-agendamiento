fbead44b4435786163721984d42f924d81f296f0
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-04-21 17:49:28 -0400

    [Alvaro] Add Login Component to use /login backend service to obtaining jwt token

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import Centers from '../Centers/Centers';
+import Login from '../Login/Login';
 import SamplePage from "../../example/SamplePage/SamplePage";
 import Availability from '../Availability/Availability';
 import TableAvailability from '../TableAvailability/TableAvailability';
@@ -11,6 +12,7 @@ export default function Routes() {
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/centros" component={Centers} />
+            <Route path="/login" component={Login} />
             <Route path="/medias" component={SamplePage} />
             <Route path="/disponibilidad" component={Availability} />
             <Route path="/tabladisponibilidad" component={TableAvailability} />

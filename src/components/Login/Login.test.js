2891b9acfa91f94364aa7b34dfa967cfc6d97d31
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-04-27 17:45:07 -0400

    [Aldemar/Alvaro] Fix login controller test

---
+++ src/components/Login/Login.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { disableFetchMocks, enableFetchMocks } from "jest-fetch-mock";
+import { act } from "react-dom/test-utils";
+import { MemoryRouter } from "react-router-dom";
+import Login from "./Login";
+
+const mockHistoryPush = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+const loginResponse = {
+  id: "1",
+  name: "fakeUsername",
+  token: "fakeToken",
+};
+
+beforeAll(() => {
+  enableFetchMocks();
+  fetch.mockResponse(() => Promise.resolve(JSON.stringify(loginResponse)));
+});
+
+afterAll(() => {
+  disableFetchMocks();
+});
+
+test("get logged through login form and redirected to home", async () => {
+  const loginForm = render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  await act(async () => {
+    fireEvent.change(loginForm.getByLabelText(/Nombre/i), {
+      target: { value: "fakeUsername" },
+    });
+    fireEvent.change(loginForm.getByLabelText(/Password/i), {
+      target: { value: "fakePassword" },
+    });
+    fireEvent.click(loginForm.getByText(/Acceder/i));
+  });
+
+  expect(localStorage.getItem("access_token")).toEqual(loginResponse.token);
+  expect(mockHistoryPush).toHaveBeenCalledWith("/");
+});

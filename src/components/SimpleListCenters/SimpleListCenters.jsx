3d5b08151399734cb84a2163850bafc2e902c8ca
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-05-11 16:58:32 -0400

    [@alcastic/@gmartinezramirez] Add simple list of centers

--- src/components/SimpleListCenters/SimpleListCenters.jsx
+++ src/components/SimpleListCenters/SimpleListCenters.jsx
@@ -1,45 +1,58 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import { httpClient } from "../../clients/httpClient";
+
+const API_URL = 'http://localhost:5000/healthcarefacilities/'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100%",
+    width: '100%',
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
-const centersNames = [
-  "ACHS Silicon Valley",
-  "ACHS Hillvalley",
-  "ACHS Abbey Road",
-];
-
-const CenterNamesList = (props) => {
-  const { centersNames } = props;
-  const listItems = centersNames.map((centerName) => (
-    <ListItem button>
-      <ListItemText primary={centerName} />
-    </ListItem>
-  ));
-  return (
-    <List component="nav" aria-label="secondary mailbox folders">
-      {listItems}
-    </List>
-  );
-};
-
-const SimpleListCenters = () => {
+export default function SelectedListItem() {
   const classes = useStyles();
+  const [selectedIndex, setSelectedIndex] = useState(1);
+  const [healthFacilities, setHealthFacilities] = useState([]);
+
+  useEffect(() => {
+    httpClient(
+      API_URL,
+      "GET",
+      (json) => {
+        setHealthFacilities(json);
+      },
+      (error) => {
+        console.log(error);
+        setHealthFacilities([]);
+      }
+    );
+  }, []);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
 
   return (
     <div className={classes.root}>
-      <CenterNamesList centersNames={centersNames} />
+      <List component="nav" aria-label="main mailbox folders">
+        { healthFacilities.map( (healthFacility, i) =>
+          <ListItem
+            button
+            selected={selectedIndex === i}
+            onClick={(event) => handleListItemClick(event, i)}
+          >
+            <ListItemText primary= {healthFacility.name} />
+        </ListItem>
+        )
+      }
+      </List>
     </div>
   );
-};
-
-export default SimpleListCenters;
+}

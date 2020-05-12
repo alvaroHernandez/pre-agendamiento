8f9e6708eb515e0f719088b7bac2323e35eff624
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-05-11 23:36:27 -0400

    [@gmartinezramirez] Display side by side components in home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import TableAvailability from "../TableAvailability/TableAvailability";
+import SimpleListCenters from "../SimpleListCenters/SimpleListCenters";
+import WelcomeAlert from "../WelcomeAlert/WelcomeAlert";
+
 import Logout from "../Logout/Logout";
+
+import Box from '@material-ui/core/Box';
 
 const Home = () => (
   <div>
     <p>Bienvenido a Pre-Agendamiento!</p>
     <Logout />
-    <TableAvailability />
+
+    <Box display="flex" flexDirection="row" p={1} m={1} bgcolor="background.paper">
+      <Box p={1} bgcolor="grey.300">
+        <SimpleListCenters />
+      </Box>
+      <Box p={1} bgcolor="grey.300">
+        <TableAvailability />
+      </Box>
+    </Box>
+
   </div>
 );
 

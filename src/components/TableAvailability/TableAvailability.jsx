4342b299722418e25acf49ebce9f1168c5a2fca0
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-05-13 23:58:19 -0400

    [Gonzalo] Add simple popup for each cell

--- src/components/TableAvailability/TableAvailability.jsx
+++ src/components/TableAvailability/TableAvailability.jsx
@@ -13,6 +13,21 @@ import hourAvailability from '../../data/HourAvailabilityType';
 
 import './tableAvailability.css';
 
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Draggable from 'react-draggable';
+
+function PaperComponent(props) {
+  return (
+    <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
+      <Paper {...props} />
+    </Draggable>
+  );
+}
 
 const HeaderDatesOfCurrentWeek = (props) => {
   const { dates } = props;
@@ -30,16 +45,61 @@ const HeaderDatesOfCurrentWeek = (props) => {
 };
 
 const BodyRowsDateAvailability = (props) => {
+  const [open, setOpen] = useState(false);
+  const [infoDescription, setInfoDescription] = useState([]);
+  const [infoText, setInfoText] = useState([]);
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const rows = createCalendarRow(props.centerCalendar, props.dates);
-  const onCellClickHandler = (key, columnName, isAvailable) => {
-    if (isAvailable === hourAvailability.AVAILABLE) {
-      isAvailable = hourAvailability.CENTER_APPOINTMENT;
-      console.log("Before was available, now not");
-      console.log(isAvailable);
+
+  const onCellClickHandler = (appointment) => {
+
+    if (appointment.type === hourAvailability.AVAILABLE) {
+      handleClickOpen();
+      setInfoDescription("Horario disponible");
+      setInfoText("El centro tiene un cupo disponible en este horario");
+    } else if (appointment.type === hourAvailability.USER_APPOINTMENT) {
+      handleClickOpen();
+      const textToDisplay = "Fecha: " + appointment.date + "\n"+ "Hora: " + appointment.hour;
+      setInfoDescription(appointment.description);
+      setInfoText(textToDisplay);
     }
+
   };
 
   return (
+    <React.Fragment>
+    <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+          {infoDescription}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+          {infoText}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
     <TableBody>
       {rows.map((row) => (
         <TableRow key={row.hour}>
@@ -53,30 +113,31 @@ const BodyRowsDateAvailability = (props) => {
             {row.hour}
           </TableCell>
           <TableCell
-            onClick={() => onCellClickHandler(row.hour, props.dates[0], row?.monday?.type)}
+            onClick={() => onCellClickHandler(row?.monday)}
             className={row?.monday?.type}
           >{(row.monday?.type!='available')?row.monday?.description:''}</TableCell>
 
           <TableCell
-            onClick={() => onCellClickHandler(row.hour, props.dates[1], row.tuesday?.type)}
+            onClick={() => onCellClickHandler(row?.tuesday)}
             className={row.tuesday?.type}
           >{(row.tuesday?.type!='available')?row.tuesday?.description:''}</TableCell>
 
           <TableCell
-            onClick={() => onCellClickHandler(row.hour, props.dates[2], row.wednesday?.type)}
+            onClick={() => onCellClickHandler(row?.wednesday)}
             className={row.wednesday?.type}
           >{(row.wednesday?.type!='available')?row.wednesday?.description:''}</TableCell>
           <TableCell
-            onClick={() => onCellClickHandler(row.hour, props.dates[3], row.thursday?.type)}
+            onClick={() => onCellClickHandler(row?.thursday)}
             className={row.thursday?.type}
           >{(row.thursday?.type!='available')?row.thursday?.description:''}</TableCell>
           <TableCell
-            onClick={() => onCellClickHandler(row.hour, props.dates[4], row.friday?.type)}
+            onClick={() => onCellClickHandler(row?.friday)}
             className={row.friday?.type}
           >{(row.friday?.type!='available')?row.friday?.description:''}</TableCell>
         </TableRow>
       ))}
     </TableBody>
+    </React.Fragment>
   );
 };
 

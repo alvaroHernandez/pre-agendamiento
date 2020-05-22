6837dd9a473d5410224fd13d24458d3ddaef495d
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-05-22 14:56:33 -0400

    [@gmartinezramirez] Add muiTheme based on institutional theme on Login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,10 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+
 import './login.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -46,40 +50,44 @@ const Login = () => {
 
   // eslint-disable-next-line no-unused-vars
   const classes = useStyles();
+  const institutionalTheme = createMuiTheme({ palette: { primary: green } });
 
   return (
-    <div className='loginStyle'>
-      <form autoComplete='off' onSubmit={handleLogin}>
-        <TextField
-          id='outlined-user'
-          required
-          label='Nombre'
-          variant='outlined'
-          value={username}
-          onChange={onChangeEventValueUsername}
-        />
-        <br />
-        <TextField
-          id='outlined-pass'
-          required
-          label='Password'
-          variant='outlined'
-          value={password}
-          onChange={onChangeEventValuePassword}
-          type='password'
-        />
-        <br />
-        <Button
-          variant='contained'
-          color='primary'
-          type='submit'
-          value='Acceder'
-        >
-          Iniciar Sesión
-        </Button>
-        <span>{error}</span>
-      </form>
-    </div>
+    <ThemeProvider theme={institutionalTheme}>
+      <div className='loginStyle'>
+        <form autoComplete='off' onSubmit={handleLogin}>
+          <TextField
+            id='outlined-user'
+            required
+            label='Nombre'
+            variant='outlined'
+            value={username}
+            onChange={onChangeEventValueUsername}
+          />
+          <br />
+          <TextField
+            id='outlined-pass'
+            required
+            label='Password'
+            variant='outlined'
+            value={password}
+            onChange={onChangeEventValuePassword}
+            type='password'
+          />
+          <br />
+          <Button
+            variant='contained'
+            color='primary'
+            type='submit'
+            value='Acceder'
+            colorOverrides={{ background: 'green' }}
+          >
+            Iniciar Sesión
+          </Button>
+          <span>{error}</span>
+        </form>
+      </div>
+    </ThemeProvider>
   );
 };
 

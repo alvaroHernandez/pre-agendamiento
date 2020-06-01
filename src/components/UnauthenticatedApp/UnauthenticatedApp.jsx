4ed3037ad2f3742277f59827b4fbc19dc5e03b0d
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2020-05-31 22:19:01 -0400

    [Alvaro] Spike for week scheduler using css grid

--- src/components/UnauthenticatedApp/UnauthenticatedApp.jsx
+++ src/components/UnauthenticatedApp/UnauthenticatedApp.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Login from '../Login/Login';
 import { Switch, Route, Router } from 'react-router-dom';
 import history from '../../services/history';
+import WeekScheduler from '../WeekScheduler/WeekScheduler';
 
 const UnauthenticatedApp = () => (
   <Router history={history}>
     <Switch>
       <Route path='/' exact component={Login} />
+      <Route path='/weekScheduler' exact component={WeekScheduler} />
     </Switch>
   </Router>
 );
